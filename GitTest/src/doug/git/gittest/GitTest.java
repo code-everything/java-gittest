698832bcdf6ec3b4136d49c2940ad7064083130d
Douglas Barrett <zppodb@gmail.com>
2015-03-09 16:36:20 -0700

    Initial Version

---
+++ GitTest/src/doug/git/gittest/GitTest.java
@@ -0,0 +1,13 @@
+package doug.git.gittest;
+
+public class GitTest {
+
+	public GitTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Git is fun but complicated yy");
+	}
+
+}
